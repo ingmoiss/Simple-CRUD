1986e5f0f1177295367c5c23a4a32c7664afb054
Moisés Navarro <moi55navacor@gmail.com>
2021-01-07 22:29:35 -0600

    Se añade etiqueta de prueba

--- src/aplicacion/Persona.java
+++ src/aplicacion/Persona.java
@@ -77,6 +77,7 @@ public class Persona extends javax.swing.JFrame {
         botonEliminar = new javax.swing.JButton();
         botonLimpiar = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         jRadioButton1.setText("jRadioButton1");
 
@@ -186,6 +187,9 @@ public class Persona extends javax.swing.JFrame {
         });
         panel.add(botonLimpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 370, 90, -1));
         panel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 420, -1, -1));
+
+        jLabel2.setText("Prueba");
+        panel.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 20, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -374,6 +378,7 @@ public class Persona extends javax.swing.JFrame {
     private javax.swing.JLabel etiquetaNacimiento;
     private javax.swing.JLabel etiquetaNombre;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JRadioButton jRadioButton1;
     private javax.swing.JPanel panel;
     // End of variables declaration//GEN-END:variables
