c0a4c9cda1f2f9c605a5c3b7f2e100f1976c6ee4
Moisés Navarro <moi55navacor@gmail.com>
2021-01-08 17:52:07 -0600

    Se borra etiqueta

--- src/aplicacion/Persona.java
+++ src/aplicacion/Persona.java
@@ -77,7 +77,6 @@ public class Persona extends javax.swing.JFrame {
         botonEliminar = new javax.swing.JButton();
         botonLimpiar = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
 
         jRadioButton1.setText("jRadioButton1");
 
@@ -187,9 +186,6 @@ public class Persona extends javax.swing.JFrame {
         });
         panel.add(botonLimpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 370, 90, -1));
         panel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 420, -1, -1));
-
-        jLabel2.setText("Prueba");
-        panel.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 20, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -378,7 +374,6 @@ public class Persona extends javax.swing.JFrame {
     private javax.swing.JLabel etiquetaNacimiento;
     private javax.swing.JLabel etiquetaNombre;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JRadioButton jRadioButton1;
     private javax.swing.JPanel panel;
     // End of variables declaration//GEN-END:variables
